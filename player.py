7e39fc07617521c1c845ab210207f75f701c8f91
SamW94 <sam.wild.1994@gmail.com>
2024-09-26 12:46:11 +0100

    create ability to shoot, with cooldown timer of 0.3s

--- player.py
+++ player.py
@@ -1,12 +1,14 @@
 import pygame
 from circleshape import *
 from constants import *
+from shot import *
 
 class Player(CircleShape):
     
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
+        self.shoot_cooldown_timer = 0
 
     # in the player class
     def triangle(self):
@@ -24,6 +26,7 @@ class Player(CircleShape):
         self.rotation += (PLAYER_TURN_SPEED * dt)
         
     def update(self, dt):
+        self.shoot_cooldown_timer -= dt
         keys = pygame.key.get_pressed()
 
         if keys[pygame.K_a]:
@@ -34,7 +37,17 @@ class Player(CircleShape):
             self.move(dt)
         if keys[pygame.K_s]:
             self.move(-abs(dt))
+        if keys[pygame.K_SPACE]:
+            if self.shoot_cooldown_timer > 0:
+                pass
+            else:
+                self.shoot()
 
     def move(self, dt):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
         self.position += forward * PLAYER_SPEED * dt
+
+    def shoot(self):
+        shot = Shot(self.position.x, self.position.y)
+        shot.velocity = pygame.Vector2(0, 1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
+        self.shoot_cooldown_timer = PLAYER_SHOOT_COOLDOWN
